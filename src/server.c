7bb830588fc1dfc740ee3e1d1e9afcaea6f77670
Simon Arys <simon.arys@student.uclouvain.be>
2022-11-07 14:29:12 +0100

    Fix bugs

--- src/server.c
+++ src/server.c
@@ -80,7 +80,7 @@ int main(int argc, char **argv) {
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET; // IPv4 
     servaddr.sin_addr.s_addr = INADDR_ANY; 
-    servaddr.sin_port = htons(listen_port);
+    servaddr.sin_port = htons(atoi(listen_port));
 
 
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) == -1) {
